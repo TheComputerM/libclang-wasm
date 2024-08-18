5868033648cb0ce716f0092fcd76741f95a7611e
Mudit Somani <mudit.somani00@gmail.com>
2024-08-18 17:09:35 +0000

    export malloc and free

--- build-libclang.sh
+++ build-libclang.sh
@@ -6,8 +6,10 @@ LLVM_SRC="$SRC/llvm-project"
 LLVM_INSTALL="$SRC/out/install"
 LIBCLANG_BIN="$SRC/out/bin"
 
+mkdir -p $LIBCLANG_BIN
+
 emcc $LLVM_INSTALL/lib/*.a --no-entry \
-    -sEXPORTED_FUNCTIONS=@$SRC/exports.txt \
+    -sEXPORTED_FUNCTIONS=@exports.txt \
     -sALLOW_MEMORY_GROWTH \
     -o $LIBCLANG_BIN/libclang.mjs
     
