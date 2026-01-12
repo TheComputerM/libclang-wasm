a5086a807a3a95283faa8499cc7747690db14e81
Mudit Somani <thecomputerm@Mudits-MacBook-Pro.local>
2026-01-13 02:17:23 +0530

    add nix flake for dev environment

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "A development shell to build libclang-wasm";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShellNoCC {
+          packages = with pkgs; [
+            emscripten
+            cmake
+            ninja
+          ];
+        };
+      }
+    );
+}
