0f6644e44246f5f49fd2734e662933b0aea4ac11
TheComputerM <thecomputerm@Mudits-MacBook-Pro.local>
2026-01-13 14:55:39 +0530

    export more runtime methods

--- build-libclang.sh
+++ build-libclang.sh
@@ -8,10 +8,12 @@ LIBCLANG_BIN="$SRC/out/bin"
 
 mkdir -p $LIBCLANG_BIN
 
+EXPORTED_METHODS=FS,wasmExports,addFunction,removeFunction,ccall,cwrap,UTF8ToString,stringToUTF8,writeArrayToMemory
+
 emcc $LLVM_INSTALL/lib/*.a --no-entry \
     -sEXPORTED_FUNCTIONS=@exports.txt \
     -sWASM_BIGINT \
     -sALLOW_MEMORY_GROWTH -sALLOW_TABLE_GROWTH \
-    -sEXPORTED_RUNTIME_METHODS=FS,wasmExports,addFunction,removeFunction,ccall,cwrap \
+    -sEXPORTED_RUNTIME_METHODS=$EXPORTED_METHODS \
     -sMODULARIZE -sEXPORT_NAME=libclang \
     -o $LIBCLANG_BIN/libclang.mjs
